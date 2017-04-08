c1248b5cfd3b6c122d4f9a0c82264cae9cf15fd5
happou31 <happo31@gmail.com>
2017-04-09 01:37:03 +0900

    video elementのチェックを追加

--- scripts/script.user.js
+++ scripts/script.user.js
@@ -8,36 +8,38 @@
 
 window.onload = () => {
     const video = document.getElementsByTagName("video")[0];
-    const f = () => {
-        const href = video.getAttribute("src");
-        if (href == null) {
-            setTimeout(f, 500);
-            return;
-        }
+    if (video != null) {
+        const f = () => {
+            const href = video.getAttribute("src");
+            if (href == null) {
+                setTimeout(f, 500);
+                return;
+            }
 
-        const filename = document.title.substr(0, document.title.length - 9);
-        const a = document.createElement("a");
-        const div = document.createElement("div");
-        div.className = "MylistCountMeta";
-        const p = document.createElement("p");
-        p.innerText = "Chrome拡張";
-        p.className = "MylistCountMeta-title";
-        
-        a.href = href;
-        a.target = "_blank";
-        a.innerText = "動画を保存";
-        a.className = "Link";
-        a.style = "font-weight: bold";
-        
-        console.log(filename);
-        a.setAttribute("download", filename);
+            const filename = document.title.substr(0, document.title.length - 9);
+            const a = document.createElement("a");
+            const div = document.createElement("div");
+            div.className = "MylistCountMeta";
+            const p = document.createElement("p");
+            p.innerText = "Chrome拡張";
+            p.className = "MylistCountMeta-title";
 
-        div.style = "text-align: center";
-        div.appendChild(p);
-        div.appendChild(a);
+            a.href = href;
+            a.target = "_blank";
+            a.innerText = "動画を保存";
+            a.className = "Link";
+            a.style = "font-weight: bold";
 
-        const page = document.getElementsByClassName("VideoMetaOverflowMenuContainer")[0];
-        page.parentNode.insertBefore(div, page);
-    };
-    setTimeout(f, 500);
+            console.log(filename);
+            a.setAttribute("download", filename);
+
+            div.style = "text-align: center";
+            div.appendChild(p);
+            div.appendChild(a);
+
+            const page = document.getElementsByClassName("VideoMetaOverflowMenuContainer")[0];
+            page.parentNode.insertBefore(div, page);
+        };
+        setTimeout(f, 500);
+    }
 }
